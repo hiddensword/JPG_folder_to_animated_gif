238dd335e2a3161c7cc9b1b191ffe8df61f87d2c
sword32@keti.re.kr <sword32@keti.re.kr>
2022-05-29 17:35:26 +0900

    GIF 정상생성 확인 완료

--- main.py
+++ main.py
@@ -3,22 +3,27 @@
 import os
 import imageio
 
-def convert(path_input, path_output, file_type, fps, save_gif_name) :
+def convert(path_input, path_output, file_type, save_gif_name) :
 
     images = []
+    count = 0
 
     for file_name in os.listdir(path_input) :
         if file_name.endswith('.{}'.format(file_type)):
 
             # 확인을 위한 사진 파일명 출력
-            print(file_name)
+            #print(file_name)
 
             file_path = os.path.join(path_input, file_name)
             images.append(imageio.imread(file_path))
+            count = count + 1   # 불러드린 사진 파일 갯수 누적
+
+    print('{} images were loaded.'.format(count))
+    print('\nPlease wait for saving your large animated GIF file.')
 
     # gif로 출력
     imageio.mimsave('{}/{}.gif'.format(path_output, save_gif_name), \
-        images, fps)
+        images, fps=10)
     print('{}.gif is saved.\n'.format(save_gif_name))
 
 
@@ -30,10 +35,11 @@ if __name__ == "__main__" :
     print("=========================")
     print("")
 
-    path_input = r'C:/DeepExpress_traversability/220529_kdy_gif/output_2022-05-10-17-06-56/frame0168' # 사진 디렉토리
-    path_output = r'C:/DeepExpress_traversability/220529_kdy_gif/output_2022-05-10-17-06-56' # 사진 디렉토리
+    #path_input = r'C:/DeepExpress_traversability/220529_kdy_gif/output_2022-05-10-17-06-56/frame0168' # 사진 디렉토리
+    path_input = r'/mnt/c/DeepExpress_traversability/gif/output_2022-05-10-17-44-45/frame0579'
+    #path_output = r'C:/DeepExpress_traversability/220529_kdy_gif/output_2022-05-10-17-06-56' # 사진 디렉토리
+    path_output = r'/mnt/c/DeepExpress_traversability/gif/output_2022-05-10-17-44-45'
     file_type = r'jpg'  # 사진 확장자
-    fps = 10 # 사진 넘기는 framerate
-    save_gif_name = r'frame0168' # 완성 gif 이름
+    save_gif_name = r'frame0579' # 완성 gif 이름
     
-    convert(path_input, path_output, file_type, fps, save_gif_name)
+    convert(path_input, path_output, file_type, save_gif_name)
